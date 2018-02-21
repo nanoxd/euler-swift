3078b7b0a46762c652dfa26b53b24e9e0dbf5982
Fernando Paredes <nano@fdp.io>
2018-02-20 21:23:29 -0500

    ✨ Add support for prime factorization

--- Euler/Euler.swift
+++ Euler/Euler.swift
@@ -7,4 +7,29 @@ final class Euler {
             return nums.union(multipliers)
         })
     }
+
+    func primeFactors(for number: Int) -> [Int] {
+        guard number >= 2 else {
+            return []
+        }
+
+        var factors = [Int]()
+        var currentNumber = number
+        var divisor = 2
+
+        while currentNumber > 1 {
+            while currentNumber % divisor == 0 {
+                factors.append(divisor)
+                currentNumber = currentNumber / divisor
+            }
+
+            divisor += 1
+        }
+
+        if currentNumber > 1 {
+            factors.append(currentNumber)
+        }
+
+        return factors
+    }
 }
--- EulerTests/EulerTests.swift
+++ EulerTests/EulerTests.swift
@@ -10,12 +10,26 @@ import XCTest
 @testable import Euler
 
 class EulerTests: XCTestCase {
+    let euler = Euler()
     func testPerformanceOfSum() {
         let array = Array.init(repeating: 2, count: 20)
 
         measure {
             let sum = array.sum()
             XCTAssertEqual(sum, 40)
+        }
+    }
+
+    func testPrimeFactors() {
+        let noFactors = euler.primeFactors(for: 1)
+        XCTAssertEqual(noFactors, [])
+
+        let factors = euler.primeFactors(for: 2)
+        XCTAssertEqual(factors, [2])
+
+        measure {
+            let primes = euler.primeFactors(for: 13_195)
+            XCTAssertEqual(primes.count, 4)
         }
     }
 }
